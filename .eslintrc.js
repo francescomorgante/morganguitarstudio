9cae39c0167cbcead37d3b12c4f8555eee9d482c
francescomorgante <prof.morgante@gmail.com>
2025-11-25 20:22:25 +0100

    chore(lib/music): add package files and config (US-011)

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = { parser: "@typescript-eslint/parser", parserOptions: { ecmaVersion: 2020, sourceType: "module", ecmaFeatures: { jsx: true } }, plugins: ["@typescript-eslint"], extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"], rules: {} };
